56fe4b500967154b030fc1e4904b5a0c9986de04
barathchittibabu <barath.c@fr8.in>
2022-03-05 14:06:28 +0530

    logic.ex.complete

--- node/adarray.js
+++ node/adarray.js
@@ -7,20 +7,19 @@ var input = {
     ], 
     branch: [ 
         { id: 1, name: "CSE" }, 
-        { id: 2, name: "IT" } 
+        { id: 2, name: "IT" }
     ], 
     student: [ 
         { id: 1, name: "Jay" }, 
-        { id: 2, name: "Sanjay" }, 
-        { id: 3, name: "Rajesh" } 
-    ] 
+        { id: 2, name: "Sanjay" },
+        { id: 3, name: "Rajesh" }
+    ]
 } 
 let a = []; 
 input.branchStudent.map((branchstudent)=>{ 
 var branch = input.branch.find((branch) => {return branchstudent.branchId == branch.id}) 
 var student = input.student.find((student)=> {return branchstudent.studentId == student.id})
 a.push({  id: branchstudent.id, branchId: branchstudent.branchId, branchName: branch.name, studentId: branchstudent.studentId, studentName: student.name}); 
- 
 })
- 
+
 console.log(a)
