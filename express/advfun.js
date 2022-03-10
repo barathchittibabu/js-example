2960965330da7b5c54c3f45b089d6ce2e186a12e
barathchittibabu <barath.c@fr8.in>
2022-03-10 18:10:08 +0530

    modify task

---
+++ express/advfun.js
@@ -0,0 +1,38 @@
+//First class citizen function
+
+//defines the add() function and assign the function name to the variable sum
+function add(a,b){
+    return a+b;
+}
+let sum = add;
+//passing a function to another function 
+function ave(a,b, fnc){
+    return fnc(a,b);
+}
+let result = ave(4,5, sum);
+console.log(result);
+//fnction returns a function that compares two objects by a property
+function compareBy(propertyName) {
+    return function (a, b) {
+      let x = a[propertyName],
+        y = b[propertyName];
+  
+      if (x > y) {
+        return 1;
+      } else if (x < y) {
+        return -1;
+      } else {
+        return 0;
+      }
+    };
+  }
+  let products = [
+      {name: 'iphone', price:900},
+      {name:'samsung', price:850},
+      {name:'xperia' , price:700}
+  ]
+  console.log('product sorted by name :');
+  products.sort(compareBy('name'));
+
+  console.table(products);
+
--- express/exp2-geturl.js
+++ express/exp2-geturl.js
@@ -27,10 +27,10 @@ app.post("/student", (req, res) => {
     let branchStudentMap = [];
   
     student_list.branchStudent.map((branchstudent)=>{ 
-        var branch = student_list.branch.find((branch) => {return branchstudent.branchId == branch.id}) 
-        var student = student_list.student.find((student)=> {return branchstudent.studentId == student.id})
-        branchStudentMap.push({  
-            id: branchstudent.id, 
+        const branch = student_list.branch.find((branch) => {return branchstudent.branchId == branch.id}) 
+        const student = student_list.student.find((student)=> {return branchstudent.studentId == student.id})
+        branchStudentMap.push({
+            id: branchstudent.id,
             branchId: branchstudent.branchId,
             branchName: branch.name, 
             studentId: branchstudent.studentId, 
