31b8e99e71be7d074896281eb63864adcb1b395f
barathchittibabu <barath.c@fr8.in>
2022-03-16 18:02:27 +0530

    resact new file

---
+++ my-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Header()
+{
+    return(
+        <h1>header</h1>  
+    )
+}
+function Barath()
+{
+    return(
+        <h1></h1>
+    )
+}
+ReactDOM.render(<Header/>,document.getElementById('root'));
